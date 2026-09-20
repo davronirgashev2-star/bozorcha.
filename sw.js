2ec469685ff4cc45001b154bf141a8ba247ecadf
davronirgashev2-star <davronirgashev2@gmail.com>
2026-09-20 13:54:23 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const V = 'bozorcha-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(V).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== V).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e => {
+  const r = e.request;
+  if (r.method !== 'GET') return;
+  const u = new URL(r.url);
+  if (u.origin !== location.origin) return;
+  if (r.mode === 'navigate') {
+    // Avval tarmoq (yangilangan katalog ko'rinsin), internet bo'lmasa kesh
+    e.respondWith(
+      fetch(r).then(res => {
+        const copy = res.clone();
+        caches.open(V).then(c => c.put('./index.html', copy));
+        return res;
+      }).catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+  e.respondWith(caches.match(r).then(m => m || fetch(r)));
+});
